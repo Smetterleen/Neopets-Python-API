4ab3fe07386261a0bb06cf1a5f6d557be1581f35
unknown <u0087393@.luna.kuleuven.be>
2013-09-22 21:33:15 +0200

    Added stockmarket api

---
+++ neopapi/games/StockMarket.py
@@ -0,0 +1,25 @@
+
+def portfolio():
+    """
+    Returns all the stocks currently in the users portfolio
+    
+    """
+    # TODO: implement
+    raise NotImplementedError
+
+def buy_stock(stock_name, amount=1000):
+    """
+    Buys the given amount of the given stock
+    
+    """
+    # TODO: implement
+    raise NotImplementedError
+
+def sell_stock(stock_name, amount=None):
+    """
+    Sells the given amount of the given stock. If no amount
+    is given, all the stocks of the given name will be sold.
+    
+    """
+    # TODO: implement
+    raise NotImplementedError
