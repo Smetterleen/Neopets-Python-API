945501c335155c2cf2461fac1c806d85578815e0
unknown <u0087393@.luna.kuleuven.be>
2013-09-22 19:38:59 +0200

    Inventory API refactor

--- neopapi/main/Exceptions.py
+++ neopapi/main/Exceptions.py
@@ -6,3 +6,7 @@ class OutOfMoneyException(Exception):
 class TooManyItemsException(Exception):
     def __init__(self):
         super(TooManyItemsException, self).__init__('You are carrying too many items')
+
+class ActionNotPossibleException(Exception):
+    def __init__(self, item_name, action):
+        super(ActionNotPossibleException, self).__init__('Action \'' + action + '\' not allowed for item: ' + item_name)
--- neopapi/main/Inventory.py
+++ neopapi/main/Inventory.py
@@ -7,6 +7,8 @@ register_page('inventory.phtml',
               True)
 register_page('quickstock.phtml')
 
+
+
 class Item(object):
     
     def __init__(self, item_id, name):
@@ -77,17 +79,7 @@ def put_in_SDB(item_list):
     it will be ignored.
     
     """
-    page = BROWSER.goto('quickstock.phtml')
-    item_trs = page.find('form', {'name': 'quickstock'}).find_all('tr')[1:]
-    item_num = 1
-    post_dict = {'buyitem': 0}
-    for item_tr in item_trs:
-        item_id = item_tr.find('input')['value']
-        item_name = item_tr.find('td').text
-        post_dict['id_arr[' + str(item_num) + ']'] = item_id
-        if item_name in item_list:
-            post_dict['radio_arr[' + str(item_num) + ']'] = 'deposit'
-    return BROWSER.post('process_quickstock.phtml', post_dict)
+    return _put_in(item_list, 'deposit')
 
 def put_in_shop(item_list):
     """
@@ -96,17 +88,7 @@ def put_in_shop(item_list):
     it will be ignored.
     
     """
-    page = BROWSER.goto('quickstock.phtml')
-    item_trs = page.find('form', {'name': 'quickstock'}).find_all('tr')[1:]
-    item_num = 1
-    post_dict = {'buyitem': 0}
-    for item_tr in item_trs:
-        item_id = item_tr.find('input')['value']
-        item_name = item_tr.find('td').text
-        post_dict['id_arr[' + str(item_num) + ']'] = item_id
-        if item_name in item_list:
-            post_dict['radio_arr[' + str(item_num) + ']'] = 'stock'
-    return BROWSER.post('process_quickstock.phtml', post_dict)
+    return _put_in(item_list, 'stock')
 
 def donate(item_list):
     """
@@ -114,17 +96,7 @@ def donate(item_list):
     is not present in the users inventory, it will be ignored.
     
     """
-    page = BROWSER.goto('quickstock.phtml')
-    item_trs = page.find('form', {'name': 'quickstock'}).find_all('tr')[1:]
-    item_num = 1
-    post_dict = {'buyitem': 0}
-    for item_tr in item_trs:
-        item_id = item_tr.find('input')['value']
-        item_name = item_tr.find('td').text
-        post_dict['id_arr[' + str(item_num) + ']'] = item_id
-        if item_name in item_list:
-            post_dict['radio_arr[' + str(item_num) + ']'] = 'donate'
-    return BROWSER.post('process_quickstock.phtml', post_dict)
+    return _put_in(item_list, 'donate')
 
 def discard(item_list):
     """
@@ -132,6 +104,41 @@ def discard(item_list):
     is not present in the users inventory, it will be ignored.
     
     """
+    return _put_in(item_list, 'discard')
+
+def put_in_gallery(item_list):
+    """
+    Attempts to put every item in the given list into the users Gallery. 
+    If a given item is not present in the users inventory, it will be ignored.
+    
+    """
+    return _put_in(item_list, 'gallery')
+
+def put_in_closet(item_list):
+    """
+    Attempts to put every item in the given list into the users Closet. 
+    If a given item is not present in the users inventory, it will be ignored.
+    
+    """
+    return _put_in(item_list, 'closet')
+
+def put_in_shed(item_list):
+    """
+    Attempts to put every item in the given list into the users Shed. 
+    If a given item is not present in the users inventory, it will be ignored.
+    
+    """
+    return _put_in(item_list, 'shed')
+
+def _put_in(item_list, place):
+    """
+    Puts the items in the given item list in the given place. Place can be:
+        deposit, stock, donate, discard, gallery, closet, shed
+    Returns a list of items that were not put in the given place, because the
+    needed action is not available for those items.
+    
+    """
+    ignored_items = []
     page = BROWSER.goto('quickstock.phtml')
     item_trs = page.find('form', {'name': 'quickstock'}).find_all('tr')[1:]
     item_num = 1
@@ -141,33 +148,9 @@ def discard(item_list):
         item_name = item_tr.find('td').text
         post_dict['id_arr[' + str(item_num) + ']'] = item_id
         if item_name in item_list:
-            post_dict['radio_arr[' + str(item_num) + ']'] = 'discard'
-    return BROWSER.post('process_quickstock.phtml', post_dict)
-
-def put_in_gallery(item_list):
-    """
-    Attempts to put every item in the given list into the users Gallery. 
-    If a given item is not present in the users inventory, it will be ignored.
+            if not item_tr.find('input', {'value': place}):
+                ignored_items.append(item_name)
+                continue
+            post_dict['radio_arr[' + str(item_num) + ']'] = place
+    return ignored_items
     
-    """
-    # TODO: implement
-    raise NotImplementedError()
-
-def put_in_closet(item_list):
-    """
-    Attempts to put every item in the given list into the users Closet. 
-    If a given item is not present in the users inventory, it will be ignored.
-    
-    """
-    # TODO: implement
-    raise NotImplementedError()
-
-def put_in_shed(item_list):
-    """
-    Attempts to put every item in the given list into the users Shed. 
-    If a given item is not present in the users inventory, it will be ignored.
-    
-    """
-    # TODO: implement
-    raise NotImplementedError()
-
